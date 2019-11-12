ec3b058fe2ac22faf56409a2adef6e1fe72f94e3
Mike Cwietniewicz <mike.cwiet@gmail.com>
2019-11-12 12:21:39 -0500

    Added an extra page to the recipe app to display details.

--- ionic_angular_demo/src/app/app-routing.module.ts
+++ ionic_angular_demo/src/app/app-routing.module.ts
@@ -3,8 +3,19 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'recipes', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomePageModule) },
-  { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesPageModule' },
+  {
+    path: 'recipes',
+    children: [
+      {
+        path: '',
+        loadChildren: './recipes/recipes.module#RecipesPageModule'
+      },
+      {
+        path: ':recipeId',
+        loadChildren: './recipes/recipe-detail/recipe-detail.module#RecipeDetailPageModule'
+      }
+    ]
+  },
 ];
 
 @NgModule({
